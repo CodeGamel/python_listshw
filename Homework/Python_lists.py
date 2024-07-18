cfeb921a74b0672209e2e3173b2626789e57ca9d
CodeGamel <collinsgamel@gmail.com>
2024-07-17 19:57:37 -0700

    First Commit

---
+++ Homework/Python_lists.py
@@ -0,0 +1,17 @@
+grades = ['85','90','78','88','76','95','89', '80','80','72','93'] 
+
+print('These grades are in numberical order')
+
+grades.sort()
+
+print(grades)
+
+nums = [85,90,78,88,76,95,89,80,80,72,93] 
+
+total = sum(nums)
+
+print("The sum of all the grades in our list is : ", total)
+
+grade_average = (926 // 11)
+
+print("This is the grade average of the class : ", grade_average)
---
+++ Homework/Slicing.py
@@ -0,0 +1,10 @@
+temp = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+
+second_week = temp[7:14]
+print(second_week)
+
+over_hundred = temp[-7:]
+print(over_hundred)
+
+more_temp = temp [5:11]
+print(more_temp)
